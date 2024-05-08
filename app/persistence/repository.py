caa84b865968ceef0d2eaa14436c502392c7a0f7
Ula <ula.malin35@gmail.com>
2024-05-08 23:50:47 +0200

    Added player, team and stadium repository

--- app/persistence/repository.py
+++ app/persistence/repository.py
@@ -5,6 +5,7 @@ import logging
 from mysql.connector.pooling import MySQLConnectionPool
 import inflection
 from datetime import date, datetime
+from app.persistence.model import Player, Team, Stadium
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,7 +22,6 @@ class CrudRepository(ABC):
 
             sql = (f'insert into {self._table_name()} ({self._column_names_for_insert()})'
                    f' values ({self._column_values_for_insert(item)})')
-            logging.info(f'INSERT SQl: {sql}')
             cursor.execute(sql)
             conn.commit()
             return cursor.lastrowid
@@ -30,6 +30,7 @@ class CrudRepository(ABC):
     def insert_many(self, items: list[Any]) -> int:
         with self._connection_pool.get_connection() as conn:
             cursor = conn.cursor()
+            print(self._table_name())
             values = ", ".join([f'({CrudRepository._column_values_for_insert(item)})' for item in items])
             sql = (f'insert into {self._table_name()} ({self._column_names_for_insert()})'
                    f' values {values}')
@@ -56,14 +57,14 @@ class CrudRepository(ABC):
             cursor = conn.cursor()
             sql = f'select count(*) from {self._table_name()}'
             cursor.execute(sql)
-            logging.info("EXECUTED")
+            # logging.info("EXECUTED")
             return cursor.fetchone()
 
     def find_all(self) -> list[Any]:
         with self._connection_pool.get_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(f'select * from {self._table_name()}')
-            logging.info("SQL EXECUTED")
+            # logging.info("SQL EXECUTED")
             return [self._entity(*row) for row in cursor.fetchall()]
 
     def find_by_id(self, id_: int) -> Any:
@@ -71,7 +72,7 @@ class CrudRepository(ABC):
             cursor = conn.cursor()
             sql = f'select * from {self._table_name()} where id_={id_}'
             cursor.execute(sql)
-            logging.info("SQL EXECUTED")
+            # logging.info("SQL EXECUTED")
             return cursor.fetchone()
 
     def delete(self, id_: int) -> int:
@@ -87,10 +88,12 @@ class CrudRepository(ABC):
             cursor = conn.cursor()
             sql = f'delete from {self._table_name()} where id_>0'
             cursor.execute(sql)
-            logging.info('exeec')
+            # logging.info('exeec')
             conn.commit()
 
     def _table_name(self) -> str:
+        # print("HERE")
+        # print(inflection.tableize(self._entity.__name__))
         return inflection.tableize(self._entity.__name__)
 
     def _field_names(self) -> list[str]:
@@ -115,3 +118,78 @@ class CrudRepository(ABC):
     def _column_names_and_values_for_update(item: Any) -> str:
         return ', '.join([f'{field}={CrudRepository._to_str(value)}' for field, value in item.__dict__.items() if
                           field.lower() != 'id_' and value is not None])
+
+
+class PlayerRepository(CrudRepository):
+    def __init__(self, connection_pool: MySQLConnectionPool):
+        super().__init__(connection_pool, Player)
+
+    def find_all_with_goals_higher_than(self, goals_limit: int) -> list[Player]:
+        with self._connection_pool.get_connection() as conn:
+            cursor = conn.cursor()
+            sql = f'select * from players p where p.goals > {goals_limit}'
+            cursor.execute(sql)
+            return [Player(*player) for player in cursor.fetchall()]
+
+    def find_all_meeting_regex(self, regex: str) -> list[Player]:
+        with self._connection_pool.get_connection() as conn:
+            cursor = conn.cursor()
+            sql = f"select * from players p where p.name REGEXP '{regex}'"
+            cursor.execute(sql)
+            return [Player(*player) for player in cursor.fetchall()]
+
+
+class TeamRepository(CrudRepository):
+    def __init__(self, connection_pool: MySQLConnectionPool):
+        super().__init__(connection_pool, Team)
+
+    def find_all_by_points_between(self, points_from: int, points_to: int) -> list[Team]:
+        with self._connection_pool.get_connection() as conn:
+            cursor = conn.cursor()
+            sql = f'select * from teams t where t.points between {points_from} and {points_to}'
+            cursor.execute(sql)
+            return [self._entity(*row) for row in cursor.fetchall()]
+
+    def find_by_name(self, name: str) -> Team:
+        with self._connection_pool.get_connection() as conn:
+            cursor = conn.cursor()
+            sql = f"select * from teams t where t.name = '{name}'"
+            cursor.execute(sql)
+            res = cursor.fetchone()
+            # logging.info("***")
+            # logging.info(res)
+            # logging.info("***")
+            return Team(*res) if res else res
+    #
+    # def update_team(self, new_team: Team):
+    #     with self._connection_pool.get_connection() as conn:
+    #         cursor = conn.cursor()
+    #         sql =
+
+
+@dataclass
+class PlayerWithTeamRepository:
+    connection_pool: MySQLConnectionPool
+
+    def find_all_players_with_teams(self, points_from: int, points_to: int):
+        with self.connection_pool.get_connection() as conn:
+            cursor = conn.cursor()
+            sql = (f'select p.* from players p join teams t on p.team_id=t.id_ '
+                   f'where t.points between {points_from} and {points_to}')
+            cursor.execute(sql)
+            return [Player(*pl) for pl in cursor.fetchall()]
+
+
+# def find_all(self):
+#     with self._connection_pool.get_connection() as conn:
+#         cursor = conn.cursor()
+#         sql = f'select * from teams'
+#         cursor.execute(sql)
+#         logging.info("SQL EXECUTED")
+#         return [Team(*row) for row in cursor.fetchall()]
+
+
+# class w
+class StadiumRepository(CrudRepository):
+    def __init__(self, connection_pool: MySQLConnectionPool):
+        super().__init__(connection_pool, Stadium)
