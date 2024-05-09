b6395b7cff5e3e780f976e122b6193ef3bb8a2fe
Ula <ula.malin35@gmail.com>
2024-05-09 23:47:42 +0200

    added instance players_with_teams_service in configuration.py

--- app/persistence/configuration.py
+++ app/persistence/configuration.py
@@ -1,5 +1,7 @@
 from app.persistence.repository import TeamRepository, PlayerRepository
 from app.persistence.connection import connection_pool
+from app.service.players_with_teams import PlayersWithTeamsService
 
 team_repo = TeamRepository(connection_pool)
 player_repo = PlayerRepository(connection_pool)
+players_with_teams_service = PlayersWithTeamsService(player_repo, team_repo)
